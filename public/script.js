30c4f3e7201ab002657d015c621cef5b972b6c9b
salim-poumki <127753285+salim-poumki@users.noreply.github.com>
2023-08-26 20:56:59 +0530

    updated all packages

--- public/script.js
+++
@@ -1,8 +0,0 @@
-window.onload=function(){
-  let menuicn = document.getElementById("menuicn");
-  let nav = document.getElementById("navcontainer");
-  menuicn.onclick=function(){
-    console.log(nav.classList)
-    nav.classList.toggle("navclose");
-  }
-}
